6b411e194019048eea46c4528f71430c46ba1f2c
Jarek Gawor <jgawor@gmail.com>
2022-09-30 10:21:14 -0400

    update to actual beta

---
+++ finish/push-instanton.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+podman tag jconf-demo-instanton docker.io/jgawor/jconf-demo-instanton
+podman push docker.io/jgawor/jconf-demo-instanton
---
+++ finish/push-og.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+podman tag jconf-demo docker.io/jgawor/jconf-demo
+podman push docker.io/jgawor/jconf-demo
+
