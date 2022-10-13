4aa69dc5c2812a87806e5dd29bcecdb790bcd276
Jarek Gawor <jgawor@gmail.com>
2022-10-13 10:02:15 -0400

    support crio and containerd

--- finish/kubernetes/start-kube.sh
+++ finish/kubernetes/start-kube.sh
@@ -10,7 +10,6 @@ export PATH=$HOME/software/go/bin:$K8REPO/kubernetes/third_party/etcd:$K8REPO/_o
 export FEATURE_GATES="AllAlpha=false"
 export CONTAINER_RUNTIME=remote
 export CGROUP_DRIVER=systemd
-export CONTAINER_RUNTIME_ENDPOINT="/var/run/crio/crio.sock"
 export ALLOW_SECURITY_CONTEXT=","
 export ALLOW_PRIVILEGED=1
 export DNS_SERVER_IP=$IP
@@ -20,6 +19,17 @@ export KUBE_ENABLE_CLUSTER_DNS=true
 export ENABLE_HOSTPATH_PROVISIONER=true
 export KUBE_ENABLE_CLUSTER_DASHBOARD=true
 export LOG_LEVEL=4
+
+if [ -f "/var/run/crio/crio.sock" ]; then
+    echo "Using CRI-O"
+    export CONTAINER_RUNTIME_ENDPOINT="/var/run/crio/crio.sock"
+elif [ -f "/run/containerd/containerd.sock" ]; then
+    echo "Using containerd"
+    export CONTAINER_RUNTIME_ENDPOINT="/run/containerd/containerd.sock"
+else
+    echo "Container runtime is not detected."
+    exit 1
+fi
 
 # export KUBELET_RESOLV_CONF=
 
