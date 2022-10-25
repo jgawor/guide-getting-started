2d54831817956a9e78e90da7946bc739718fee88
Jarek Gawor <jgawor@gmail.com>
2022-10-25 01:04:47 -0400

    support local images

--- finish/knative/knative-services.sh
+++ finish/knative/knative-services.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-kubectl apply -f knative-og.yaml
-kubectl apply -f knative-instanton.yaml
+REGISTRY="${1:-docker.io/jgawor}"
+
+cat knative-og.yaml | sed "s|image: docker.io/jgawor|image: $REGISTRY|" | kubectl apply -f -
+cat knative-instanton.yaml | sed "s|image: docker.io/jgawor|image: $REGISTRY|" | kubectl apply -f -
 
 kubectl port-forward service/kourier -n kourier-system 8080:80 &
